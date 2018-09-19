298cd8e85558a620568c8e870bbf00f7d9fa7d37
Michael William Muller <michael.w.muller@aptiv.com>
2018-09-19 13:50:47 -0400

    Added spacer and renamed varaibles

--- Python/gpio.py
+++ Python/gpio.py
@@ -11,7 +11,6 @@ hostname = '127.0.0.1'
 username = 'root'
 password = 'root'
 dbname = 'python'
-pinCur = 0
 
 #this object will store each appliance? 
 class appliance(object):
@@ -29,11 +28,11 @@ class appliance(object):
 def doQuery( conn ):
 	cur = conn.cursor()
 	conn.commit()
-	cur.execute( "SELECT id, name, gpioPin, pinState FROM test" )
+	cur.execute( "SELECT id, appname, pin, flag FROM test" )
 
-	for id, name, gpioPin, pinState  in cur.fetchall() :
+	for id, appname, pin, flag  in cur.fetchall() :
 		#print "Id: %s; Name: %s; PIN #%s ; State: %s" %(id, name, gpioPin, pinState)
-		parseAppliance(id, name, gpioPin, pinState)
+		parseAppliance(id, appname, pin, flag)
 		
 
 #this method will pull from the database every half second and update the GPIO pins
@@ -41,16 +40,16 @@ def dbThread( conn ):
 	endconn = "False"
 	while 1:
 		time.sleep(.05)
-		#print "Time Update..."
+		print "============================================="
 		doQuery( conn )
 
 #Used to test pin state against current state and then send pin to get changed in GPIO function
 def parseAppliance(id, name, pin, state):
 	global pinCur # Temp variable. Will be removed once we have pi and will use the
 	# getpinState function to get value
-	if state != pinCur:
-		print "Appliance %s using PIN %s has changed to %s." %(name, pin, state)
-		pinCur = state
+	#if state != pinCur:
+	#	print "Appliance %s using PIN %s has changed to %s." %(name, pin, state)
+	#	pinCur = state
 
 	#Used for testing the current state of pin against the state from the database to look for changes. (No use without Pi)
 	#oldState = getpinState()
@@ -61,10 +60,10 @@ def parseAppliance(id, name, pin, state):
 		#Nothing because the state didn't change
 	
 	#Stream of table in database as its refreshed
-	#if state == 0:
-	#	print "Appliance %s using PIN %s is 'Off'." %(name, pin)
-	#else:
-	#	print "Appliance %s using PIN %s is 'On'." %(name, pin)
+	if state == 0:
+		print "Appliance %s using PIN %s is 'Off'." %(name, pin)
+	else:
+		print "Appliance %s using PIN %s is 'On'." %(name, pin)
 
 #Pulls the state of the pin for comparison		
 def getpinState(pinnum):
