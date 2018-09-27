08d41f20c6dfa489a2824746c0accde3d6383305
Michael Muller <mwmuller@oakland.edu>
2018-09-26 21:13:48 -0400

    GPIO library swap and Optomizations
    Switched from gpiozero to RPi.GPIO Library so the state of the pin was more easily traceable and the pin would stay on until turned off. Removed unnecessary code to cut down the load on the Pi

--- Python/gpio.py
+++ Python/gpio.py
@@ -1,8 +1,8 @@
 #This python code will run the server on the Raspberry pi and handle the incoming messages from the client. We will be using GPIO pins to control the flow of power in the system. 
 
-import threading, subprocess, pickle
+import threading, subprocess
 import mysql.connector
-#from gpiozero import LED
+import RPi.GPIO as gpio
 from mysql.connector import MySQLConnection, Error
 
 #import RPi.GPIO as GPIO
@@ -13,7 +13,6 @@ username = 'root'
 password = 'root'
 dbname = 'SeniorProject'
 dbtable = 'Addon'
-count = 0
 #this object will store each appliance? 
 class appliance(object):
 	id = ""
@@ -29,38 +28,28 @@ class appliance(object):
 
 #Makes a query request to the database and passes returned data to parser
 def doQuery( conn ):
-	global count
 	cur = conn.cursor(buffered=True)
-	conn.commit()
-	cur.execute( "SELECT id, appname, pin, flag FROM test WHERE EXISTS (Select table_schema,table_name,update_time FROM information_schema.tables WHERE update_time > (NOW() - INTERVAL .15 SECOND) AND table_schema = '%s' AND table_name='%s')" %(dbname, dbtable));
-	if cur.rowcount > 0:
-		count = count + 1
-		print "%s ========================================" %(count)
+	conn.commit()	
+	#For now we are just pulling all the time until we can recompile the newest version of mysql to 5.7 on ARM64.
+	cur.execute( "Select Addon_ID, Addon_Name, Addon_Pin, Addon_State from Addon")
+	#cur.execute( "SELECT Addon_ID, Addon_Name, Addon_Pin, Addon_State FROM %s WHERE EXISTS (Select table_schema,table_name,update_time FROM information_schema.tables WHERE update_time > (NOW() - INTERVAL .15 SECOND) AND table_schema = '%s' AND table_name='%s')" %(dbtable, dbname, dbtable));
+	if cur.rowcount > 0 : 
+		print "========================================"
 		for Addon_ID, Addon_Name, Addon_Pin, Addon_State in cur.fetchall() :
-			parseAppliance(Addon_ID, Addon_Name, Addon_Pin, Addon_State)
-
+			GPIO(Addon_Pin, Addon_State)
+			#parseAppliance(Addon_ID, Addon_Name, Addon_Pin, Addon_State)
 
 #this method will pull from the database every half second and update the GPIO pins
 def dbThread( conn ):
-	endconn = "False"
-	
 	while 1:
 		time.sleep(.1)
 		doQuery( conn )
 
 #Used to test pin state against current state and then send pin to get changed in GPIO function
 def parseAppliance(id, name, pin, state):
-	#Used for testing the current state of pin against the state from the database to look for changes. (No use without Pi)
-	#oldState = getpinState()
-	#if state != oldState: # Assuming it returns a 0 or 1
-	#	print "Changing appliance %s at PIN %s from %s to %s!" %(name, ping, oldState, state)
-	#	GPIO(pin, state)
-	#else:
-		#Nothing because the state didn't change
-	
 	#Stream of table in database as its refreshed
 	print "Flag of appliance %s using PIN %s is %s." %(name, pin, state)
-	#GPIO(pin, state)
+	GPIO(pin, state)
 
 
 
@@ -71,22 +60,20 @@ def MySQLConnect():
 	# Pull from the database and sync the pin states to that of the database.
 	t = threading.Thread(target=dbThread, args=(myConnection,))
 	t.start()
-	#myConnection.close()	
 
 #Sends messages to the GPIO Pins
 def GPIO(pinNum, On_Off):
-	print "blahj"
-	#try: 
-		#led = LED(pinNum)
-		#This statement handles the state which the LED is on
-		#Right now the inverse is used to turn the light off.
-		#if On_Off == 1:
-			#led.on()
-		#else:
-			#led.off()
-		#return "Pin %s has is now %s!" %(pin, On_Off)
-	#except:	
-		#print "Blah"
+	#Setting up the GPIO Pin that is being used
+	gpio.setmode(gpio.BCM)
+	gpio.setwarnings(False)
+	gpio.setup(pinNum, gpio.OUT)
+	#print "State of Pin %s" %(gpio.input(pinNum)) #Debugging
+	#Right now the inverse is used to turn the light off.
+	if On_Off == 1 and gpio.input(pinNum) == 0 :
+		gpio.output(pinNum, gpio.HIGH)
+	elif On_Off == 0 and gpio.input(pinNum) == 1:
+		gpio.output(pinNum, gpio.LOW)
+	#print "Pin %s is now %s!" %(pinNum, On_Off) #Debugging
 
 
 if __name__ =='__main__':
