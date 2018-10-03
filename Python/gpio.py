cfa5b093d90fe8ae28d1053615da5ca9a27531ff
Michael Muller <mwmuller@oakland.edu>
2018-10-03 07:00:13 -0700

    Working with Global Dims
    First test case for implementing Dimmable lights by storing the dimmed gpio pin in a global array to keep its state when the function is exited.

--- Python/gpio.py
+++ Python/gpio.py
@@ -12,6 +12,7 @@ password = 'root'
 dbname = 'SeniorProject'
 dbtable = 'Addon'
 
+dLights = [] # This array will hold the dimmed lights so the pins stay dimmed when function is exited.
 
 #Makes a query request to the database and passes returned data to parser
 def doQuery( conn ):
@@ -20,11 +21,10 @@ def doQuery( conn ):
 	#For now we are just pulling all the time until we can recompile the newest version of mysql to 5.7 on ARM64.
 	cur.execute( "Select Addon_ID, Addon_Name, Addon_Pin, Addon_State from Addon")
 	#cur.execute( "SELECT Addon_ID, Addon_Name, Addon_Pin, Addon_State FROM %s WHERE EXISTS (Select table_schema,table_name,update_time FROM information_schema.tables WHERE update_time > (NOW() - INTERVAL .15 SECOND) AND table_schema = '%s' AND table_name='%s')" %(dbtable, dbname, dbtable)); #This query might not be needed. The impact would be minimal
-	if cur.rowcount > 0 : 
-		#print "========================================"
+	if cur.rowcount > 0 : #Check if any rows were returned
 		try:
-			for Addon_Pin, Addon_State in cur.fetchall() :
-				GPIO(Addon_Pin, Addon_State)
+			for Addon_ID, Addon_Pin, Addon_State in cur.fetchall() :
+				GPIO(Addon_ID, Addon_Pin, Addon_State)
 			#for Addon_Pin, Addon_State, Addon_Dim, Addon_dimVal in cur.fetchall() :
 				#GPIO(Addon_Pin, Addon_State, Addon_Dim, Addon_dimVal)
 		except:
@@ -48,24 +48,23 @@ def MySQLConnect():
 
 #Sends messages to the GPIO Pins
 #Use this header if adding Dim.
-def GPIO(pinNum, On_Off):
+def GPIO(id, pinNum, On_Off):
 	#Setting up the GPIO Pin that is being used
 	gpio.setmode(gpio.BCM)
 	gpio.setwarnings(False)
 	gpio.setup(pinNum, gpio.OUT)
-	#Setting up the current pin for dimming
-		#pwm=gpio.PWM(pinNum, 1000)
-	#Right now the inverse is used to turn the light off.
+
+	#Checks the value of the pin against current state
 	if On_Off == 1 and gpio.input(pinNum) < 1 :
 		gpio.output(pinNum, gpio.HIGH)
 	elif On_Off == 0 and gpio.input(pinNum) > 0:
 		gpio.output(pinNum, gpio.LOW)
 	elif On_Off != 1 and On_Off != 0:
-		#At this point the value might be used for dimming
+		#At this point the value might be used for dimming (Assume 0 < x < 1)
+		#global dLights
 		#pwm = gpio.PWM(pinNum, gpio.OUT)
-		#pwm.ChangeDutyCycle(dimVal)
-		
-		print "sit"
+		#pwm.ChangeDutyCycle(On_Off * 100)
+		#dLights[id] = pwm
 
 if __name__ =='__main__':
 	#gpio.setmode(gpio.BCM)
