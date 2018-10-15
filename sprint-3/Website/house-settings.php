43dbf93bda46e7d71831464661b65611fcb2abe6
Tream <amanaila@oakland.edu>
2018-10-14 22:05:06 -0400

    room list auto-populated on load; +, -, and submit buttons work

--- sprint-3/Website/house-settings.php
+++ sprint-3/Website/house-settings.php
@@ -15,6 +15,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
         <link href="CSS/main.css" type="text/css" rel="stylesheet">
+        <link rel="icon" type="image/png" href="Images/harp_icon.png">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     
@@ -44,55 +45,108 @@
                 errorText.classList.add('lbl-setup-house-visible');
             }
         }
+        
+        function fnReturnHome() {
+            if(confirm('Are you sure? Any changes you made will not be saved.'))
+                document.location.href = 'index.php';
+        }
+        
+        function addNewRoomBox() {
+            var box = document.createElement("div");
+                box.setAttribute("class", "col-md-12");
+                box.setAttribute("id", "new");
+            var icn = document.createElement("i");
+                icn.setAttribute("class", "fa fa-map-marker fa-login");
+            var inp = document.createElement("input");
+                inp.setAttribute("type", "text");
+                inp.setAttribute("placeholder", "Room Name");
+                inp.setAttribute("class", "input-settings");
+                inp.setAttribute("name", "newRooms[]");
+                inp.setAttribute("style", "margin:4px");
+            var btn = document.createElement("i");
+                btn.setAttribute("class", "fa fa-minus fa-settings-remove-room");
+                btn.setAttribute("onclick", "removeRoomBox(this.parentNode)");
+            box.appendChild(btn);
+            box.insertBefore(inp, btn);
+            box.insertBefore(icn, inp);
+            
+            document.getElementById("myForm").insertBefore(box, document.getElementById("buttonDiv"));
+        }
+        
+        function removeRoomBox(target=-1) {
+            if (target == -1) return;
+            var id = target.id;
+            if (id == "new") {
+                document.getElementById("myForm").removeChild(target);   
+            } else {
+                var inp = target.getElementsByTagName("input")[0];
+                inp.setAttribute("name", "delRooms[]");
+                inp.setAttribute("value", "" + target.id);
+                target.style.display = "none";
+            }
+        }
     </script>
     
     <body class="login-body">
         <div class="component-settings-form-container">
-            <form>
-                <h1 class="col-md-12 text-center h1-settings">House Configuration</h1>
-                <label id="houseErrorText" class="lbl-setup-house-hidden"></label>
+            <button class="btn-back" onclick="fnReturnHome()"><i class="fa fa-arrow-left" style="font-size: 10px;"></i> <i class="fa fa-home"></i></button>
+            <h1 class="text-center h1-settings">House Configuration</h1>
+                <?php
+                    if (isset($_SESSION['home'])) {
+                        $hID = $_SESSION['home'];
+                        $sql = "select * from Room where House_ID='$hID'";
+                        $result = mysqli_query($conn,$sql);
+                        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+                        $count = mysqli_num_rows($result);
+
+                        $sql2 = "select * from House where House_ID='$hID'";
+                        $result2 = mysqli_query($conn,$sql2);
+                        $row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+
+                        if (isset($_SESSION['houseSetMsg'])) {
+                            echo '<label id="houseErrorText" class="lbl-setup-house-visible">' . $_SESSION['houseSetMsg'] . '</label>';
+                            unset($_SESSION['houseSetMsg']);
+                        } else {
+                            echo '<label id="houseErrorText" class="lbl-setup-house-hidden"></label>';
+                        }
+                ?>
+                <form id="myForm" action="house-settingsScript.php" method="post">
                 <div class="col-md-12">
                     <i class="fa fa-home fa-login"></i>
-                    <input type="text" placeholder="House Name" class="input-login">
+                <?php
+                        echo '<input type="text" placeholder="House Name" value="' . $row2['House_Name'] . '" class="input-login" style="margin:1px" name="houseName">
+                              <i class="fa fa-plus fa-settings-add-room" onclick="addNewRoomBox()"></i>';
+                ?>
                 </div>
                 <?php
-                    $hID = $_SESSION['home'];
-                    $sql = "select * from Rooms where Username='$hID'";
-                    $result = mysqli_query($conn,$sql);
-                    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-                    $count = mysqli_num_rows($result);
-                
-                    if ($count == 0) {
-                        echo '<div class="col-md-12">
-                                <i class="fa fa-map-marker fa-login"></i>
-                                <input type="text" placeholder="Room Name" class="input-settings">
-                                <i class="fa fa-plus fa-settings-add-room"></i>
-                                <i class="fa fa-minus fa-settings-remove-room"></i>
-                              </div>';
-                    } else {
-                        
+                        if ($count == 0) {
+
+                            echo '<div class="col-md-12">
+                                    <i class="fa fa-map-marker fa-login"></i>
+                                    <input type="text" placeholder="Room Name" class="input-settings" name="newRooms[]">
+                                  </div>';
+                        } else {
+
+                            echo '<div id="' . $row['Room_ID'] . '" class="col-md-12">
+                                        <i class="fa fa-map-marker fa-login"></i>
+                                        <input type="text" placeholder="Room Name" value="' . $row['Room_Name'] . '" class="input-settings" name="updateRooms[' . $row['Room_ID'] . '][]">
+                                        <i class="fa fa-minus fa-settings-remove-room" onclick="removeRoomBox(this.parentNode)"></i>
+                                      </div>';
+                            while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+
+                                echo '<div id="' . $row['Room_ID'] . '" class="col-md-12">
+                                        <i class="fa fa-map-marker fa-login"></i>
+                                        <input type="text" placeholder="Room Name" value="' . $row['Room_Name'] . '" class="input-settings" name="updateRooms[' . $row['Room_ID'] . '][]">
+                                        <i class="fa fa-minus fa-settings-remove-room" onclick="removeRoomBox(this.parentNode)"></i>
+                                      </div>';
+                            }
+                        }
+                    } else { 
+                        echo '<label id="houseErrorText" class="lbl-setup-house-visible">You are not assigned to a house.</label>';
                     }
                 ?>
-                <div class="col-md-12">
-                    <i class="fa fa-map-marker fa-login"></i>
-                    <input type="text" placeholder="Room Name" class="input-settings">
-                    <i class="fa fa-plus fa-settings-add-room"></i>
-                    <i class="fa fa-minus fa-settings-remove-room"></i>
-                </div>
-                <div class="col-md-12">
-                    <i class="fa fa-map-marker fa-login"></i>
-                    <input type="text" placeholder="Room Name" class="input-settings">
-                    <i class="fa fa-plus fa-settings-add-room"></i>
-                    <i class="fa fa-minus fa-settings-remove-room"></i>
-                </div>
-                <div class="col-md-12">
-                    <i class="fa fa-map-marker fa-login"></i>
-                    <input type="text" placeholder="Room Name" class="input-settings">
-                    <i class="fa fa-plus fa-settings-add-room"></i>
-                    <i class="fa fa-minus fa-settings-remove-room"></i>
-                </div>
-                <div class="btn-login-float">
-                    <button formaction="submit" class="btn-save-house">Save</button>
+                <div id="buttonDiv" class="btn-login-float">
+                    <button type="submit" class="btn-save-house">Save</button>
                 </div>
             </form>
         </div>
