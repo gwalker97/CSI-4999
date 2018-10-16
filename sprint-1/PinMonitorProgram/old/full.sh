2a26820d0c2b031a71cb4db31dba29fa0cc11b7c
Tream <amanaila@oakland.edu>
2018-10-16 10:24:18 -0400

    cleanup; these are still in my branch

--- sprint-1/PinMonitorProgram/old/full.sh
+++
@@ -1,1 +0,0 @@
-curl "http://192.168.128.146/toolsSuite/index.php?action=saveresult&controller=Main&id=1&status=1"
--- sprint-1/PinMonitorProgram/old/notfull.sh
+++
@@ -1,1 +0,0 @@
-curl "http://192.168.128.146/ToolsSuite/index.php?action=saveresult&controller=Main&id=1&status=0"
