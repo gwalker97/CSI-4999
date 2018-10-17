6008bc8a75d3ffb7c1422c35049090dc41d09083
mwmuller <mwmuller@oakland.edu>
2018-10-16 21:03:27 -0400

    Editing the new Arduino Files
    working on getting the esp8266 to read and activate pins.

--- Hardware Code/Arduino/sketch_jan27b/sketch_jan27b.ino
+++ Hardware Code/Arduino/sketch_jan27b/sketch_jan27b.ino
@@ -3,25 +3,23 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
-#include <Ethernet.h>
 #include <MySQL_Connection.h>
+#include <MySQL_Cursor.h>
 
 // Replace with your network credentials
 const char* ssid = "Galaxy S6 active 6927";
 const char* password = "qjxu4534";
 IPAddress server_addr(192,168,43,219);  // IP of the MySQL *server* here
-char user[] = "root";              // MySQL user login username
-char dbpass[] = "root";
+char* user = "root";              // MySQL user login username
+char* dbpass = "root";
 
 WiFiClient client;
 MySQL_Connection conn((Client *)&client);
 
 
 void setup(void){
-//  webPage += "<h1>ESP8266 Web Server</h1>";
-//  webPage += "<p>Socket #2 <a href=\"socket2On\"><button>ON</button></a>&nbsp;<a href=\"socket2Off\"><button>OFF</button></a></p>";
-
-  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
   // preparing GPIOs
   Serial.begin(115200);
   WiFi.begin(ssid, password);
@@ -39,33 +37,37 @@ void setup(void){
   Serial.println(WiFi.localIP());
   Serial.print("MAC Address: ");
   Serial.println(WiFi.macAddress());
-if(conn.connect(server_addr, 3306, user, dbpass)){
+while(!conn.connect(server_addr, 3306, user, dbpass)){
   delay(1000);
-  Serial.println("You have connected to the database");
-}else{
-  Serial.println("Connection Failed");
-  conn.close();
+  //Serial.println(".");
 }
-
+Serial.println("You have connected");
 }
  
 void loop(void){
-  Serial.println("\nRunning SELECT and printing results\n");
-
+  int totaltime = 0;
+  delay(50);
+  totaltime += 50;
+  //Serial.println("\nRunning SELECT and printing results\n");
+  if(totaltime % 200 == 0){
+    digitalWrite(LED_BUILTIN, LOW);
+  }else{
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
   // Initiate the query class instance
   MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-  char* query = "Select * from SeniorProject.Addon";
+  char* query = "Select Addon_MAC, Addon_Pin, Addon_State from SeniorProject.Addon";
   cur_mem->execute(query);
   // Fetch the columns and print them
   column_names *cols = cur_mem->get_columns();
   // printing the column names.
   for (int f = 0; f < cols->num_fields; f++) {
-    Serial.print(cols->fields[f]->name);
+    //Serial.print(cols->fields[f]->name);
     if (f < cols->num_fields-1) {
-      Serial.print(", ");
+      //Serial.print(", ");
     }
   }
-  Serial.println();
+  //Serial.println();
   // Read the rows and print them
   row_values *row = NULL;
   do {
@@ -74,15 +76,19 @@ void loop(void){
       for (int f = 0; f < cols->num_fields; f++) {
         // Printing each value. Send the values to the gpio function for pin manipulation
         // Send specific index to the gpio function. As of now, check the ip, index length-1, and index length-2
-        Serial.print(row->values[f]);
+        //Serial.print(row->values[f]);
+        char* mac = row->values[0];
+        if(mac == WiFi.macAddress()){
+          gpio(row->values[1], row->values[2]);
+        }
         if (f < cols->num_fields-1) {
-          Serial.print(", ");
+         // Serial.print(", ");
         }
       }
-      Serial.println();
+      //Serial.println();
     }
   } while (row != NULL);
-  // Deleting the cursor also frees up memory used
+  //Serial.println("Deleting Cursor");
   delete cur_mem;
 }
 
@@ -90,12 +96,11 @@ void loop(void){
 void gpio(int pin, float state){
   pinMode(pin, OUTPUT);
   if(state == 1 && digitalRead(pin) < 1){
-    digitalWrite(pin, LOW)
-  }else if{state == 0 && digitalRead(pin) > 0){
-    digitalWrite(pin, HIGH)
+    digitalWrite(pin, LOW);
+  }else if(state == 0 && digitalRead(pin) > 0){
+    digitalWrite(pin, HIGH);
   }else{
     int turns = state * 255;
     analogWrite(pin, turns);
   }
-
 }
