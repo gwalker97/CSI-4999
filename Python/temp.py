9201ea46366b6c9498c7f93522ac7ce4d4c6b4b8
Michael Muller <mwmuller@oakland.edu>
2018-10-18 13:29:01 -0700

    awd

--- Python/temp.py
+++ Python/temp.py
@@ -27,18 +27,18 @@ def read_sensor():
 			device_file = device_folder + '/w1_slave'
 			done = True
 		except:	
-			done = False 
+			done = False
 	return done
 
 def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
+	f = open(device_file, 'r')
+	lines = f.readlines()
+        f.close()
+        return lines
  
 def read_temp():
 	global c, f
-	#try:
+	try:
     		lines = read_temp_raw()
      		while lines[0].strip()[-3:] != 'YES':
        			time.sleep(0.2)
@@ -50,9 +50,9 @@ def read_temp():
         			temp_string = lines[1][equals_pos+2:]
         			c = float(temp_string) / 1000.0
         			f = temp_c * 9.0 / 5.0 + 32.0
-	return 0, 0
-	#except:
-		#print read_sensor()
+		return c, f
+	except:
+		print read_sensor()
 
 def mysqlConn():
 	myconn = mysql.connector.connect(host=hostname, user=username, passwd=password, db=dbname)
