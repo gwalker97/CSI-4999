23d72a70758b1e531c9c74e3171b2da26b4b67bf
mwmuller <mwmuller@oakland.edu>
2018-10-19 21:54:48 -0400

    Updated Arduino sketch

--- Hardware Code/Arduino/sketch_jan27b/sketch_jan27b.ino
+++ Hardware Code/Arduino/sketch_jan27b/sketch_jan27b.ino
@@ -12,14 +12,16 @@ const char* password = "qjxu4534";
 IPAddress server_addr(192,168,43,219);  // IP of the MySQL *server* here
 char* user = "root";              // MySQL user login username
 char* dbpass = "root";
-
+int LED = 2;
 WiFiClient client;
 MySQL_Connection conn((Client *)&client);
+bool connected = false;
 
 
+//setup function
 void setup(void){
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
+  
+  pinMode(LED, OUTPUT);
   // preparing GPIOs
   Serial.begin(115200);
   WiFi.begin(ssid, password);
@@ -30,6 +32,7 @@ void setup(void){
     delay(500);
     Serial.print(".");
   }
+  connected = true;
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
@@ -41,22 +44,31 @@ while(!conn.connect(server_addr, 3306, user, dbpass)){
   delay(1000);
   //Serial.println(".");
 }
+
+ MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
+ cur_mem->execute("use SeniorProject");
+ dbconn = true;
 Serial.println("You have connected");
 }
  
 void loop(void){
-  int totaltime = 0;
-  delay(50);
-  totaltime += 50;
-  //Serial.println("\nRunning SELECT and printing results\n");
-  if(totaltime % 200 == 0){
-    digitalWrite(LED_BUILTIN, LOW);
-  }else{
-    digitalWrite(LED_BUILTIN, HIGH);
+  if(connected){
+    digitalWrite(LED, HIGH);
   }
+ delay(50);
+ digitalWrite(LED, LOW);
+ if(WiFi.status() != WL_CONNECTED){
+  connected = false;
+ }
+  //Serial.println("\nRunning SELECT and printing results\n");
+  /*if(totaltime % 200 == 0){
+    analogWrite(LED_BUILTIN, 100);
+  }else{
+    analogWrite(LED_BUILTIN, 100);
+  }*/
   // Initiate the query class instance
   MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-  char* query = "Select Addon_MAC, Addon_Pin, Addon_State from SeniorProject.Addon";
+  char* query = "SELECT Hosts.Host_Mac, Addon.Addon_Pin, Addon.Addon_State, Addon.Addon_Type from Addon INNER JOIN Hosts on Addon.Addon_Host_ID = Hosts.Host_ID;";
   //char* query = "Select * from SeniorProject.Addon";
   cur_mem->execute(query);
   // Fetch the columns and print them
@@ -82,7 +94,7 @@ void loop(void){
          // Serial.println(row->values[0]);
           // Serial.println(WiFi.macAddress());
         if(mac == WiFi.macAddress()){
-          gpio(atoi(row->values[1]), atof(row->values[2]));
+          gpio(atoi(row->values[1]), atof(row->values[2]), row->values[3]);
          // Serial.println("Made it here");
        }
         if (f < cols->num_fields-1) {
