da8b5b682fa3d14ca567c7df5446713386ff17dc
Michael Muller <mwmuller@oakland.edu>
2018-10-23 08:34:14 -0700

    Testing auto Cooling

--- Python/cooling.py
+++ Python/cooling.py
@@ -1,3 +1,20 @@
 import num
+import RPi.GPIO as gpio
+import temp
 
-print "%s" %(num.getnum())
+pin = 17
+
+
+def coolOn():
+	global pin
+	gpio.setmode(gpio.BCM)
+	gpio.setwarnings(False)
+	gpio.setup(pin, gpio.OUT)
+	gpio.output(pin, gpio.HIGH)
+
+def coolOff():
+	global pin
+	gpio.setmode(gpio.BCM)
+	gpio.setwarnings(False)
+	gpio.setup(pin, gpio.OUT)
+	gpio.output(pin, gpio.LOW)
