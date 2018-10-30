350f461593e41b09f5f9992c428e324d1ce2674f
danzerdude10 <jrdanz@oakland.edu>
2018-10-30 11:55:16 -0400

    style group settings page, change name of files

--- Website/groupSettings.php
+++ Website/groupSettings.php
@@ -28,7 +28,7 @@
         
         function addNewGroupBox() {
             var box = document.createElement("div");
-                box.setAttribute("class", "col-md-12");
+                box.setAttribute("class", "col-lg-12 col-md-12 col-sm-12 col-xs-12");
                 box.setAttribute("id", "new");
             var icn = document.createElement("i");
                 icn.setAttribute("class", "fa fa-lock fa-login");
@@ -36,7 +36,7 @@
                 inp.setAttribute("type", "text");
                 inp.setAttribute("class", "input-settings");
                 inp.setAttribute("name", "newGroup[]");
-                inp.setAttribute("style", "width:70%;margin:4px");
+                inp.setAttribute("style", "margin:4px");
             var btn = document.createElement("i");
                 btn.setAttribute("class", "fa fa-minus fa-settings-remove-room");
                 btn.setAttribute("onclick", "removeGroupBox(this.parentNode)");
@@ -93,75 +93,55 @@
                     }
             
                     if (isset($_SESSION['accountSetMsg'])) {
-                        echo '<label id="houseErrorText" class="lbl-setup-house-visible">' . $_SESSION['accountSetMsg'] . '</label>';
+                        echo '<label id="houseErrorText" class="lbl-setup-house-visible col-lg-12 col-md-12 col-sm-12 col-xs-12">' . $_SESSION['accountSetMsg'] . '</label>';
                         unset($_SESSION['accountSetMsg']);
                     } else {
-                        echo '<label id="houseErrorText" class="lbl-setup-house-hidden"></label>';
+                        echo '<label id="houseErrorText" class="lbl-setup-house-hidden col-lg-12 col-md-12 col-sm-12 col-xs-12"></label>';
                     }
                 ?>
-                <form id="myForm" action="accountSettingsScript.php" method="post">
-                <?php
-                    echo '<div id="' . $_SESSION['uID'] . '" class="col-md-12">
-                            <i class="fa fa-user fa-login"></i>
-                            <input style="width:60%" type="text" value="' . $_SESSION['user'] . '" class="input-login" style="margin:1px" name="updateAccounts[' . $_SESSION['uID'] .'][]">
-                            <i class="fa fa-plus fa-settings-add-room" onclick="addNewGroupBox()"></i>
-                            <i class="fa fa-minus fa-settings-remove-room" onclick="removeAccountBox(this.parentNode)"></i>';
-                        echo '<select style="background-color:black;margin-left:4px" class="fa fa-lock fa-login" name="updateAccounts[' . $_SESSION['uID'] . '][]">';
-                            foreach ($groups as $row) {
-
-                                if ($_SESSION['gID'] == $row['Groups_gID']) {
-                                    echo '<option selected value="' . $row['Groups_gID'] . '">' . $row['Groups_Name'] .'</option>';
-                                } else {
-                                    echo '<option value="' . $row['Groups_gID'] . '">' . $row['Groups_Name'] .'</option>';
-                                }
-                            }
-                    echo '</select></div>';
-                    
-                    $sql2 = "select * from Groups where Groups_gID>2";
-                    $result2 = mysqli_query($conn,$sql2);
-                ?>
-                    
-                    <div id="groupList" style="margin:5%">
-                        <p class="text-center" style="margin-bottom:-1%"><b>Custom Groups</b></p>
+                <form id="myForm" action="groupSettingsScript.php" method="post">
+                    <div id="groupList" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p class="text-center p-user"><b>Custom Groups</b></p>
 
                         <?php
-
+                            $sql2 = "select * from Groups where Groups_gID>2";
+                            $result2 = mysqli_query($conn,$sql2);
+                        
                             if (mysqli_num_rows($result2) == 0) {
                                 echo '<p>There are no custom groups.</p>';
                             } else {
 
                                 while ($row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC)) {
-                                    echo '<div id="' . $row2['Groups_gID'] . '" class="col-md-12">
+                                    echo '<div id="' . $row2['Groups_gID'] . '" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                             <i class="fa fa-lock fa-login"></i>
-                                            <input style="width:80%" type="text" placeholder="Room Name" value="' . $row2['Groups_Name'] . '" class="input-settings" name="updateGroups[' . $row2['Groups_gID'] . '][]">
+                                            <input type="text" placeholder="Room Name" value="' . $row2['Groups_Name'] . '" class="input-settings" name="updateGroups[' . $row2['Groups_gID'] . '][]">
                                             <i class="fa fa-minus fa-settings-remove-room" onclick="removeGroupBox(this.parentNode)"></i>
                                           </div>';
                                 }
+                            }
                         ?>
 
                     </div>
 
                     <?php
-                        }
-
                         $sql3 = "select * from User where not User_ID=" . $_SESSION['uID'];
                         $result3 = mysqli_query($conn,$sql3);
                     ?>
 
-                    <div id="accountList">
-                        <p class="text-center" style="margin-bottom:-1%"><b>Other Accounts</b></p>
+                    <div id="accountList col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p class="text-center p-user col-lg-12 col-md-12 col-sm-12 col-xs-12"><b>Other Accounts</b></p>
                         <?php
 
                             if (mysqli_num_rows($result3) == 0) {
-                                echo '<p>There are no other accounts.</p>';
+                                echo '<p class="col-lg-12 col-md-12 col-sm-12 col-xs-12">There are no other accounts.</p>';
                             } else {
 
                                 while ($row3 = mysqli_fetch_array($result3,MYSQLI_ASSOC)) {
-                                    echo '<div id="' . $row3['User_ID'] . '" class="col-md-12">
+                                    echo '<div id="' . $row3['User_ID'] . '" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                             <i class="fa fa-user fa-login"></i>
-                                            <input style="width:55%" type="text" value="' . $row3['Username'] . '" class="input-settings" name="updateAccounts[' . $row3['User_ID'] . '][]">
+                                            <input style="width:56%" type="text" value="' . $row3['Username'] . '" class="input-settings" name="updateAccounts[' . $row3['User_ID'] . '][]">
                                             <i class="fa fa-minus fa-settings-remove-room" onclick="removeAccountBox(this.parentNode)"></i>
-                                            <select style="background-color:black" class="fa fa-lock fa-login" name="updateAccounts[' . $row3['User_ID'] . '][]">';
+                                            <select class="fa fa-lock fa-login user-dropdown-2" name="updateAccounts[' . $row3['User_ID'] . '][]">';
                                                 foreach ($groups as $row) {
 
                                                     if ($row3['User_gID'] == $row['Groups_gID']) {
@@ -170,13 +150,12 @@
                                                         echo '<option value="' . $row['Groups_gID'] . '">' . $row['Groups_Name'] .'</option>';
                                                     }
                                                 }
-                                    echo '</select>
-                                          </div>';
+                                    echo '</select><i class="fas fa-caret-down user-caret"></i></div>';
                                 }
                             }
                         ?>
                     </div>
-                <div id="buttonDiv" class="btn-login-float">
+                <div id="buttonDiv" class="btn-login-float col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <button type="submit" class="btn-save-house">Save</button>
                 </div>
             </form>
