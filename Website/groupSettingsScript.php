7edf78c8762356913c056c6218ca5aa21258b9a9
Tream <amanaila@oakland.edu>
2018-11-06 13:52:10 -0500

    cleaned up code, everything should work

--- Website/groupSettingsScript.php
+++ Website/groupSettingsScript.php
@@ -8,29 +8,30 @@
     }
 
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
-        print(var_dump($_POST));
-        
+
         if ($_SESSION['gID'] == 1) {
-        
+
             //updateAccounts
             if (array_key_exists('updateAccounts', $_POST)) {
-                $sql = "select * from User";
+                $sql = "select * from User where not User_ID=" . $_SESSION['uID'];
                 $result = mysqli_query($conn,$sql);
 
                 while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-                    $uName = mysqli_real_escape_string($conn, $_POST['updateAccounts'][$row['User_ID']][0]);
-                    $gNum = mysqli_real_escape_string($conn, $_POST['updateAccounts'][$row['User_ID']][1]);
 
-                    if ($uName == mysqli_real_escape_string($conn, $row['Username']) and $gNum != $row['User_gID']) {
-                        $sql2 = "update User set User_gID='" . $gNum . "' where Username=" . $row['Username'];
-                        $result2 = mysqli_query($conn,$sql2);
+                    if (array_key_exists($row['User_ID'], $_POST['updateAccounts'])) {
+                        $gNum = mysqli_real_escape_string($conn, $_POST['updateAccounts'][$row['User_ID']][0]);
 
-                        if ($result2 === false) {
+                        if ($gNum != $row['User_gID']) {
+                            $sql2 = "update User set User_gID='" . $gNum . "' where User_ID=" . $row['User_ID'];
+                            $result2 = mysqli_query($conn,$sql2);
 
-                            if (isset($_SESSION['groupSetMsg'])) {
-                                $_SESSION['groupSetMsg'] .= "<br>Could not update [" . $row['Username'] . "'s group.";
-                            } else {
-                                $_SESSION['groupSetMsg'] = "Could not update [" . $row['Username'] . "'s group.";
+                            if ($result2 === false) {
+
+                                if (isset($_SESSION['groupSetMsg'])) {
+                                    $_SESSION['groupSetMsg'] .= "<br>Could not update [" . $row['Username'] . "]'s group.";
+                                } else {
+                                    $_SESSION['groupSetMsg'] = "Could not update [" . $row['Username'] . "]'s group.";
+                                }
                             }
                         }
                     }
@@ -39,29 +40,124 @@
             
             //updateGroups
             if (array_key_exists('updateGroups', $_POST)) {
-                $sql3 = "select * from Groups";
+                $sql3 = "select * from Groups where Groups_gID>2";
                 $result3 = mysqli_query($conn,$sql3);
 
                 while($row3 = mysqli_fetch_array($result3,MYSQLI_ASSOC)) {
-                    $uName = mysqli_real_escape_string($conn, $_POST['updateAccounts'][$row['User_ID']][0]);
-                    $gNum = mysqli_real_escape_string($conn, $_POST['updateAccounts'][$row['User_gID']][1]);
+                    
+                    if (array_key_exists($row3['Groups_gID'], $_POST['updateGroups'])) {
+                        $gName = mysqli_real_escape_string($conn, $_POST['updateGroups'][$row3['Groups_gID']][0]);
 
-                    if ($uName == mysqli_real_escape_string($conn, $row['Username']) and $gNum != $row['User_gID']) {
-                        $sql2 = "update User set User_gID='" . $gNum . "' where Username=" . $row['Username'];
-                        $result2 = mysqli_query($conn,$sql2);
+                        if ($gName != mysqli_real_escape_string($conn, $row3['Groups_Name'])) {
+                            $sql4 = "update Groups set Groups_Name='" . $gName . "' where Groups_gID=" . $row3['Groups_gID'];
+                            $result4 = mysqli_query($conn,$sql4);
 
-                        if ($result2 === false) {
+                            if ($result4 === false) {
 
-                            if (isset($_SESSION['groupSetMsg'])) {
-                                $_SESSION['groupSetMsg'] .= "<br>Could not update [" . $row['Username'] . "'s group.";
-                            } else {
-                                $_SESSION['groupSetMsg'] = "Could not update [" . $row['Username'] . "'s group.";
+                                if (isset($_SESSION['groupSetMsg'])) {
+                                    $_SESSION['groupSetMsg'] .= "<br>Could not update group [" . $row3['Groups_Name'] . "].";
+                                } else {
+                                    $_SESSION['groupSetMsg'] = "Could not update group [" . $row3['Groups_Name'] . "].";
+                                }
                             }
                         }
                     }
                 }
             }
             
+            //newGroups
+            if (array_key_exists('newGroups', $_POST)) {
+                
+                foreach ($_POST['newGroups'] as $val) {
+                    $val2 = mysqli_real_escape_string($conn, $val);
+                    $sql5 = "insert into Groups (Groups_Name) values ('" . $val2 . "')";
+                    $result5 = mysqli_query($conn,$sql5);
+                    
+                    if ($result5 === false) {
+                        
+                        if (isset($_SESSION['groupSetMsg'])) {
+                            $_SESSION['groupSetMsg'] .= "<br>Could not create group [" . $val2 . "].";
+                        } else {
+                            $_SESSION['groupSetMsg'] = "Could not create group [" . $val2 . "].";
+                        }
+                    }
+                }
+            }
+            
+            //delGroups
+            if (array_key_exists('delGroups', $_POST)) {
+                
+                $success = false;
+                
+                foreach ($_POST['delGroups'] as $val) {
+                    $val2 = mysqli_real_escape_string($conn, $val);
+                    
+                    //Rooms table
+                    $sql6 = "update Room set Room_gID=2 where Room_gID=" . $val2;
+                    $result6 = mysqli_query($conn,$sql6);
+                    
+                    if ($result6 === true) {
+                        
+                        //User table
+                        $sql7 = "update User set User_gID=2 where User_gID=" . $val2;
+                        $result7 = mysqli_query($conn,$sql7);
+                        
+                        if ($result7 === true) {
+                            
+                            //safe to remove
+                            $sql8 = "delete from Groups where Groups_gID=" . $val2;
+                            $result8 = mysqli_query($conn,$sql8);
+                            
+                            if ($result8 === true) {
+                                $success = true;
+                            }
+                        }
+                    }
+                    
+                    if ($success === false) {
+                        
+                        if (isset($_SESSION['groupSetMsg'])) {
+                            $_SESSION['groupSetMsg'] .= "<br>Could not delete group [" . $val2 . "].";
+                        } else {
+                            $_SESSION['groupSetMsg'] = "Could not delete group [" . $val2 . "].";
+                        }
+                    }
+                }
+            }
+            
+            //delAccounts
+            if (array_key_exists('delAccounts', $_POST)) {
+                
+                $success = false;
+                
+                foreach ($_POST['delAccounts'] as $val) {
+                    $val2 = mysqli_real_escape_string($conn, $val);
+                    
+                    //House_Assignment table
+                    $sql9 = "delete from House_Assignment where Assign_User_ID=" . $val2;
+                    $result9 = mysqli_query($conn,$sql9);
+                    
+                    if ($result9 === true) {
+                        $sql10 = "delete from User where User_ID=" . $val2;
+                        $result10 = mysqli_query($conn,$sql10);
+
+                        if ($result10 === true) {
+                            $success = true;
+                        }
+                    }
+                    
+                    if ($success === false) {
+
+                        if (isset($_SESSION['groupSetMsg'])) {
+                            $_SESSION['groupSetMsg'] .= "<br>Could not delete account [" . $val2 . "].";
+                        } else {
+                            $_SESSION['groupSetMsg'] = "Could not delete account [" . $val2 . "].";
+                        }
+                    }
+                }
+            }
+            
+            header("Location: groupSettings.php");
         } else { 
             
             if (isset($_SESSION['indexMsg'])) {
@@ -71,61 +167,15 @@
             }
             
             header("Location: index.php");
-            
         }
-/*        
-        //update Accounts
-        if (array_key_exists('updateAccounts', $_POST)) {
-            $sql = "select * from User";
-            $result = mysqli_query($conn,$sql);
+    } else { 
+
+        if (isset($_SESSION['groupSetMsg'])) {
+            $_SESSION['groupSetMsg'] .= "<br>Method must be POST.";
+        } else {
+            $_SESSION['groupSetMsg'] = "Method must be POST.";
+        }
         
-            while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-
-                if (array_key_exists($row['User_ID'], $_POST['updateAccounts'])) {
-                    $rName = mysqli_real_escape_string($conn, $_POST['updateAccounts'][$row['User_ID']][0]);
-                    $rNum = mysqli_real_escape_string($conn, $_POST['updateAccounts'][$row['User_gID']][1]);
-
-                    if ($rName != mysqli_real_escape_string($conn, $row['Username']) or $rNum != $row['User_gID']) {
-
-                        if ($_SESSION['gID'] == 1 or ($_SESSION['uID'] == $row['User_ID'])) {
-                            
-                            if ($_SESSION['gID'] == 1) {
-                                
-                                $sql = "update Room set Room_Name='" . $rName . "', Room_gID='" . $rNum . "' where Room_ID=" . $row2['Room_ID'];
-                            } else {
-                                $sql3 = "update Room set Room_Name='" . $rName . "' where Room_ID=" . $row2['Room_ID'];
-                                
-                                if ($rNum != $row2['Room_gID']) {
-                                    
-                                    if (isset($_SESSION['houseSetMsg'])) {
-                                        $_SESSION['houseSetMsg'] .= "<br>You cannot change [" . $rName . "]'s group.";
-                                    } else {
-                                        $_SESSION['houseSetMsg'] = "You cannot change [" . $rName . "]'s group.";
-                                    }
-                                }
-                            }
-                            $result3 = mysqli_query($conn,$sql3);
-
-                            if (result3 === false) {
-                                
-                                if (isset($_SESSION['houseSetMsg'])) {
-                                    $_SESSION['houseSetMsg'] .= "<br>Could not change [" . $row2['Room_Name'] . "]'s name.";
-                                } else {
-                                    $_SESSION['houseSetMsg'] = "Could not change [" . $row2['Room_Name'] . "]'s name.";
-                                }
-                            }
-                        } else {
-                            
-                            if (isset($_SESSION['houseSetMsg'])) {
-                                $_SESSION['houseSetMsg'] .= "<br>You cannot edit [" . $row2['Room_Name'] . "].";
-                            } else {
-                                $_SESSION['houseSetMsg'] = "You cannot edit [" . $row2['Room_Name'] . "].";
-                            }
-                        }
-                    }  
-                }
-            }
-        }
-*/
+        header("Location: groupSettings.php");
     }
 ?>
