d149cd2ab1f6bd31d1947ee4d1e08601a45be4bd
Tream <amanaila@oakland.edu>
2018-12-04 13:44:55 -0500

    fixed indexMsg, addons in Scenes can now be deleted

--- Website/deleteComponent.php
+++ Website/deleteComponent.php
@@ -1,17 +1,25 @@
 <?php
 require('config.php');
 
-$_SESSION['indexMsg'] = "Component deleted!";
-
 $id = mysqli_real_escape_string($conn, $_POST['id']);
 
-$sql = "DELETE from Addon WHERE Addon_ID = '$id'";
+$sql = "delete from Scene_Assignment where Addon_ID = " . $id;
+$result = mysqli_query($conn,$sql);
+$sql2 = "DELETE from Addon WHERE Addon_ID = '$id'";
+$result2 = mysqli_query($conn,$sql2);
 
+if ($result2 === true) {
+    $_SESSION['indexMsg'] = "Component deleted!";
+} else {
+    $_SESSION['indexMsg'] = "Component could not be deleted!";
+}
+/*
 if ($conn->query($sql) === TRUE) {
     echo "Page saved!";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+*/
 $conn->close();
 ?>
 
